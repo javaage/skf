3fdf9e8b0e0cf4179136abe5fa9de703dfce05e6
hb_java@sina.com <Java19786028>
2018-06-29 09:40:19 +0800

    pyc

--- python-server/flask-test.py
+++ python-server/flask-test.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from a import method_test
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return method_test()
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8888, debug=True)

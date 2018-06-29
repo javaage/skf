3ba114aa6d54a1c8c80aed8c85f3c181bea8c785
hb_java@sina.com <Java19786028>
2018-06-29 10:23:57 +0800

    simple uwsgi

--- python-server/flask-test.py
+++ python-server/flask-test.py
@@ -1,11 +1,10 @@
 from flask import Flask
-from a import method_test
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return method_test()
+    return "hello"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8888, debug=True)
